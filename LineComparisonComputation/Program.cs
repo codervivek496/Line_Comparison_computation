3ed27d82dd0622dfb544c33b930f97d2a07091e0
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-13 06:49:17 -0700

    Line comparison computation

---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
